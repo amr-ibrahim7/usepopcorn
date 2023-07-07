a435e1e1c8d34ebbb9b0ec739bd0df3f1c783b49
amr-ibrahim7 <amrnew100@gmail.com>
2023-07-07 20:28:03 +0300

    adding useEffect with Handling Errors

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import StarRating from './StarRating';
-// import './index.css';
-// import App from './App';
+// import StarRating from './StarRating';
+import './index.css';
+import App from './App';
+
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating color='blue' maxRating={10} onSetRating={setMovieRating} />
+//       <p>This Movie was Rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating />
+    <App />
+    {/* <StarRating
+      maxRating={5}
+      messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+    /> */}
+    {/* <StarRating size={24} color='red' className='test' defaultRating={2} />
+    <Test /> */}
   </React.StrictMode>
 );
